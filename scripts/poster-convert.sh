2f9dde9b94f910bc73cdcaa42050df9c3ea07496
Gabriel A. Devenyi <gdevenyi@gmail.com>
2014-12-11 16:26:24 -0500

    Fix poster convert

--- scripts/poster-convert.sh
+++ scripts/poster-convert.sh
@@ -1,1 +1,1 @@
-for file in posters/*.pdf; do convert -density 25 -background white -flatten $file $(basename $file pdf)jpg; done
+for file in posters/*.pdf; do convert -density 25 -background white -flatten $file posters/$(basename $file pdf)jpg; done
