9d08b941df036c5fd5d1e2ade6491fe55cf65bb5
Gabriel A. Devenyi <gdevenyi@gmail.com>
2015-07-21 17:38:38 -0400

    Automagically generate citation list from pubmed

--- scripts/generate-citations.sh
+++ scripts/generate-citations.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
 #Needs an input file http://www.bioinformatics.org/texmed/ using "(((Chakravarty, Mallar[Author]) OR Chakravarty, Mallar M[Author]) OR Chakravarty, M[Author]) OR Chakravarty, M Mallar[Author] "
-../bib2xhtml-v3.0-15-gf506/bib2xhtml -u -r -c -s empty ../citations-pubmed.bib ../CobraLab.github.io/publications/index.html
-sed -i 's/M.*M.*Chakravarty/<strong>M. M. Chakravarty<\/strong>/g' ../CobraLab.github.io/publications/index.html
+#Get Ids
+IDS=$(curl -G http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi --data "retmax=100000&db=pubmed&term=(((Chakravarty,+Mallar[Author])+OR+Chakravarty,+Mallar+M[Author])+OR+Chakravarty,+M[Author])+OR+Chakravarty,+M+Mallar[Author]" | grep "<Id>" | sed -r 's/<.{0,1}Id>//g' | tr -d '\t' | paste -sd,)
+
+curl -G http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi --data "db=pubmed&retmode=xml&id=${IDS}" | xsltproc --novalid pubmed2bibtex.xsl - > citations.bib
+
+bib2xhtml -u -r -c -s empty -n Chakravarty citations.bib ../publications/index.html
+rm citations.bib
